1ce1ab93105eebbe2d9ec9185a25aeb683b4106e
RajanBakshi238 <rajan.bakshi238@gmail.com>
2026-03-08 13:01:33 +0530

    feat: update Academy module to include User, Role, and Permission entities for enhanced functionality

--- src/modules/academy/academy.module.ts
+++ src/modules/academy/academy.module.ts
@@ -3,11 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Academy } from './academy.entity';
 import { AcademyController } from './academy.controller';
 import { AcademyService } from './academy.service';
+import { Permission } from '../permission/permission.entity';
+import { User } from '../user/user.entity';
+import { Role } from '../role/role.entity';
+import { AbilityFactory } from '@/common/casl/ability.factory';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Academy])],
-    controllers: [AcademyController],
-    providers: [AcademyService],
-    exports: [AcademyService],
+  imports: [
+    TypeOrmModule.forFeature([Academy, User, Role, Permission]),
+  ],
+  controllers: [AcademyController],
+  providers: [AcademyService, AbilityFactory],
+  exports: [AcademyService],
 })
-export class AcademyModule { }
+export class AcademyModule {}
